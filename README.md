00a9c161063fd253fdbabf35b8cf30bdaed8c967
Tony Rasa <trasa@meancat.com>
2025-01-03 16:12:49 -0800

    initial commit of redis-lab

---
+++ README.md
@@ -0,0 +1,5 @@
+# REDIS LAB
+
+Part 2 of the CS 261 Redis Lab. You will be modifying this code to
+use a local Redis server instead of storing everything in memory.
+
---
+++ redis-lab.js
@@ -0,0 +1,50 @@
+const port = 13100;
+
+// build a consistent response object
+function buildResult(userId, score) {
+    let result = {};
+    result["userId"] = userId;
+    result["score"] = score;
+    return result;
+}
+
+// create the Express server
+import express from 'express';
+const app = express();
+app.use(express.json());
+
+// our score repository, which we will replace with Redis...
+const allScores = {};
+
+// post a score for a user, writing over any previous score
+app.post('/scoresByUser/:userId', (req, res) => {
+    if (!req.body.score) {
+        console.log("No req.body.score found; did you forget to update putter & the postman json script?");
+        res.sendStatus(400);
+        return;
+    }
+    allScores[req.params.userId] = req.body.score; 
+    let result = buildResult(req.params.userId, req.body.score);
+    console.log(`Created score for user ${result.userId}: ${result.score}`);
+    res.send(result);   
+});
+
+// get the score stored for this user
+app.get('/scoresByUser/:userId', (req, res) => {
+    let score = allScores[req.params.userId];
+    if (score == null) {
+        res.sendStatus(404);
+        return;
+    }
+    let result = buildResult(req.params.userId, score);
+    res.send(result);
+});
+
+// start the server
+app.listen(port, (error) => {
+    if (error != null) {
+        console.log(`Error starting Express server on port ${port}: ${error}`);
+    } else {
+        console.log(`Listening on port ${port}`);
+    }
+});
